4aa9b7548be58855f2f1b5bc7285eda9246c92ee
Thang <dthangnguyen19@gmail.com>
2024-06-10 07:59:27 +0000

    codecrafters submit [skip ci]

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,67 +1,107 @@
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <cstring>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <algorithm>
 #include <arpa/inet.h>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
 #include <netdb.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <unordered_set>
+#include <vector>
 
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
+  // You can use print statements as follows for debugging, they'll be visible
+  // when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
   // Uncomment this block to pass the first stage
   //
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
+    std::cerr << "Failed to create server socket\n";
+    return 1;
   }
-  
+
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) <
+      0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(6379);
-  
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) !=
+      0) {
     std::cerr << "Failed to bind to port 6379\n";
     return 1;
   }
-  
+
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
     std::cerr << "listen failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
-  
-  std::cout << "Waiting for a client to connect...\n";
-  
-  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
 
+  std::cout << "Waiting for a client to connect...\n";
+
+  std::vector<int> client_fds;
   while (true) {
-    char buffer[1024]{0};
-    recv(client_fd, buffer, sizeof(buffer), 0);
-    send(client_fd, "+PONG\r\n", 7, 0);
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(server_fd, &readfds);
+    int nfds = server_fd;
+    for (const auto client_fd : client_fds) {
+      FD_SET(client_fd, &readfds);
+      nfds = std::max(nfds, client_fd);
+    }
+    int activity = select(nfds + 1, &readfds, nullptr, nullptr, nullptr);
+    if (activity == 0) {
+      std::cout << "Select error\n";
+    } else {
+      if (FD_ISSET(server_fd, &readfds)) {
+        auto new_client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+                                    (socklen_t *)&client_addr_len);
+        client_fds.emplace_back(new_client_fd);
+        std::cout << "New client connected, fd = " << new_client_fd << "\n";
+      }
+      std::unordered_set<int> to_remove_client_fds;
+      for (const auto client_fd : client_fds) {
+        if (FD_ISSET(client_fd, &readfds)) {
+          char buffer[1024]{0};
+          auto valread = read(client_fd, buffer, sizeof(buffer));
+          if (valread == 0) {
+            close(client_fd);
+            to_remove_client_fds.emplace(client_fd);
+            std::cout << "Client disconnected\n";
+          } else {
+            send(client_fd, "+PONG\r\n", 7, 0);
+          }
+        }
+      }
+      client_fds.erase(std::remove_if(client_fds.begin(), client_fds.end(),
+                                      [&to_remove_client_fds](const auto &x) {
+                                        return to_remove_client_fds.find(x) !=
+                                               to_remove_client_fds.end();
+                                      }),
+                       client_fds.end());
+    }
   }
-  std::cout << "Client connected\n";
-  
+
   close(server_fd);
 
   return 0;
